0dbf355f9984b554f186e27f43f5db044e2ed390
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-06-18 09:29:37 +0530

    init: init shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
+    <div className="flex flex-col space-y-5 items-center justify-center h-screen w-screen">
       <h1 className="text-4xl font-bold">Task Management App</h1>
+      <br />
+      <Button onClick={() => alert("Clicked")}>Click me</Button>
     </div>
   );
 }
